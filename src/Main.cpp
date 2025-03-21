a79856dcfe3022fba1a18ee9078bf8cec983abf3
dCUBExBYdtCUBE <119281142+dCUBExBYdtCUBE@users.noreply.github.com>
2025-03-21 10:31:05 +0530

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,26 +9,7 @@ int main()
     return 0;
 }
 
-// Game.hpp
-#pragma once
-#include <SFML/Graphics.hpp>
-#include "Player.hpp"
-#include "Map.hpp"
-#include "RayCaster.hpp"
 
-class Game
-{
-private:
-    sf::RenderWindow window;
-    Player player;
-    Map map;
-    RayCaster raycaster;
-    sf::Clock clock;
-    bool isRunning;
-
-public:
-    Game(int width, int height, const std::string& title);
-    void run();
     void handleInput();
     void update(float deltaTime);
     void render();
